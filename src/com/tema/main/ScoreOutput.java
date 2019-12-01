4aca0cee4ccf0dde84681ab6b24ebde662c78525
oana-diaconescu <oanadiaconescu7@gmail.com>
2019-12-01 13:24:50 +0200

    improved input method and implemented Visitor for output writing

---
+++ src/com/tema/main/ScoreOutput.java
@@ -0,0 +1,73 @@
+package com.tema.main;
+
+import com.tema.abilities.FightMode;
+import com.tema.abilities.PlayerVisitor;
+import com.tema.input.Battlefield;
+import com.tema.input.GameInfo;
+import com.tema.players.*;
+import fileio.FileSystem;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ScoreOutput implements PlayerVisitor {
+    private final FileWriter fileWriter;
+
+    public ScoreOutput(FileWriter fileWriter) throws IOException {
+        this.fileWriter = fileWriter;
+    }
+
+    public void printPlayerDetails(Player player) throws IOException {
+        if(player.getLevel() == -1) {
+            fileWriter.write("dead\n");
+        } else {
+            fileWriter.write(player.getLevel() + " " + player.getCurrentHP() + " " + player.getCurrentXP() + " " +
+                    player.getrowPos() + " " + player.getcolumnPos() + '\n');
+        }
+    }
+
+
+    public void visit(Pyromancer pyromancer){
+        try {
+
+            fileWriter.write("P ");
+            printPlayerDetails(pyromancer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void visit(Rogue rogue){
+        try {
+
+            fileWriter.write("R ");
+            printPlayerDetails(rogue);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void visit(Wizard wizard){
+        try {
+
+            fileWriter.write("W ");
+            printPlayerDetails(wizard);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void visit(Knight knight){
+        try {
+
+            fileWriter.write("K ");
+            printPlayerDetails(knight);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
